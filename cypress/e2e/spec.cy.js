d45cada2a83518aef10c2dbd330868e7eae7d3e0
Calli Herrmann <calliherrmann@gmail.com>
2023-08-07 13:12:08 -0400

    Add test to add new order

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -34,4 +34,23 @@ describe("user navigation", () => {
     .get('section').find('div').last().find('h3').should('have.text', 'Bobby')
     .get('.ingredient-list').find('li').last().should('have.text', 'cheese')
   });
+  it.only('Should allow user to add a new order', () => {
+    cy.intercept('POST', 'http://localhost:3001/api/v1/orders', {
+      statusCode: 200,
+      fixture: 'newOrder'
+    }).as('postNewOrder')
+    cy.visit("http://localhost:3000/")
+    .wait('@allOrders')
+    .get('input[name="name"]').type('Sam').should('have.value', 'Sam')
+    .get('button[name="steak"]').click()
+    .get('button[name="hot sauce"]').click()
+    .get('header').find('p').should('have.text', 'Order: steak, hot sauce')
+    .get('button').contains('Submit Order').click()
+    .wait('@postNewOrder')
+    .get('input[name="name"]').should('have.value', '')
+    .get('header').find('p').should('have.text', 'Order: Nothing selected')
+    .get('section').find('div').first().find('h3').should('have.text', 'Lydia')
+    .get('section').find('div').last().find('h3').should('have.text', 'Sam')
+    .get('.ingredient-list').find('li').last().should('have.text', 'hot sauce')
+  })
 });
