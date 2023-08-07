fc36398f26a1fa0b8b81e65b1105f8b0114f4f15
Calli Herrmann <calliherrmann@gmail.com>
2023-08-07 12:38:01 -0400

    Add test to stub get request on page load with fixture

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,14 @@
-describe("empty spec", () => {
-  it("passes", () => {
-    cy.visit("https://example.cypress.io");
+describe("user navigation", () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'http://localhost:3001/api/v1/orders', {
+      statusCode: 200,
+      fixture: 'orders'
+    })
+  })
+  it("Should load page", () => {
+    cy.visit("http://localhost:3000/")
+    .get('h1').should('have.text', 'Burrito Builder')
+    .get('input[name="name"]').should('have.value', '')
+    
   });
 });
