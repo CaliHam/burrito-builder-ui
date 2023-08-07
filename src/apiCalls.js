8d8b04d7eeb90cdc5450a17c24e1a59e5e8530f8
Calli Herrmann <calliherrmann@gmail.com>
2023-08-07 11:23:35 -0400

    Add postcall to apicalls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,12 @@
 export const getOrders = () => {
   return fetch("http://localhost:3001/api/v1/orders")
   .then((response) => response.json())
-};
+}
+
+export const postOrder = (newOrder) => {
+  return fetch("http://localhost:3001/api/v1/orders", {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: {newOrder}
+  }).then(response => response.json())
+}
