94821dbabfc40a1064985cb095a4d26fc3403af3
Calli Herrmann <calliherrmann@gmail.com>
2023-08-07 12:44:33 -0400

    Add test for ingredients buttons

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -6,9 +6,26 @@ describe("user navigation", () => {
     })
   })
   it("Should load page", () => {
+    const ingredients = [
+      "beans",
+      "steak",
+      "carnitas",
+      "sofritas",
+      "lettuce",
+      "queso fresco",
+      "pico de gallo",
+      "hot sauce",
+      "guacamole",
+      "jalapenos",
+      "cilantro",
+      "sour cream",
+    ];
     cy.visit("http://localhost:3000/")
     .get('h1').should('have.text', 'Burrito Builder')
     .get('input[name="name"]').should('have.value', '')
+    ingredients.map(ingredient => {
+      cy.get(`button[name="${ingredient}"]`).should('have.value', `${ingredient}`)
+    })
     
   });
 });
